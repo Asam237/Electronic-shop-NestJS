505b29b6741d2a1a1cf5a7bfcdd861feb7662ea4
Asam237 <abbasalimokolo@gmail.com>
2022-11-27 07:16:42 +0100

    feat: add module in app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DB_URL } from 'common/vars';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register(),
+    EventEmitterModule.forRoot(),
+    MongooseModule.forRoot(DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
